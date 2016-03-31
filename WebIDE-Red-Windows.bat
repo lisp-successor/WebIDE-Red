ad4a52a49111cb34f649f720c262aa003909fbde
lisp-successor <lisp-successor@outlook.com>
2016-03-31 08:55:30 +0800

    Update WebIDE-Red-Windows.bat

--- WebIDE-Red-Windows.bat
+++ WebIDE-Red-Windows.bat
@@ -1,1 +1,9 @@
+@ECHO OFF
+:: use ._exe or .lol to avoid exeute directly
+taskkill /f /t /im rebol-view-278-3-1.lol
+:: timeout /t 0.5
+start .\core\windows\rebol-view-278-3-1.lol -i -v -s .\core\webide.reb
+EXIT
 
+:: WebIDE-Windows.lnk
+:: %comspec% /C Core\windows\rebol-view-278-3-1.exe -i -v -s Core\webide.r
